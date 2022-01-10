96aa61dc3da83091cd846424013985bb0fb1f6e4
Peter Vatistas <petervatistas@protonmail.com>
2022-01-09 22:56:03 -0700

    spacing

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false});
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   // set window size
